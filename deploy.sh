af5eb1a90b1df9575456953790139b6f3a99f42f
light <light0x00@163.com>
2022-02-28 19:36:45 +0800

    chore: 发布脚本upd

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ mvn versions:set -DnewVersion=$1
 export GPG_TTY=$(tty) && mvn clean deploy -P ossrh,release
 git add -u
 git commit -m "release: v-$1"
-git tag "v-$1"
+git tag -a "v-$1" -m "release version $1"
